45576516b09836018719e5c7cfe68cb1d70ccb51
Lucas Ferrari <lucas.carrijoferrari@gmail.com>
2025-03-18 17:20:42 -0300

    init

---
+++ racionix-spring/src/main/java/io/github/lucaswithboots/racionixspring/RacionixSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.lucaswithboots.racionixspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacionixSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RacionixSpringApplication.class, args);
+    }
+
+}
