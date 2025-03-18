79ea4a3785e7905b437e87e0455feaa84f5dbe40
lucas ferrari <lucas.carrijoferrari@gmail.com>
2025-03-18 08:25:48 -0300

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router";
 import Games from "./pages/Games";
 import Header from "./components/navigation/Header";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
     return (
         <>
             <Header />
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/games" element={<Games />} />
                 <Route path="/login" element={<Login />} />
             </Routes>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <div className="bg-red-400 h-screen w-screen  grid grid-cols-2"></div>
+    );
+}
